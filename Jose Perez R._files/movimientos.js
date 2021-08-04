8a3c6714bf8d7cf2228aff7b5432f59af9928375
Jose Luis <joseluis15_1997@hotmail.com>
2021-08-03 22:48:55 -0500

    Primera version de mi perfil

---
+++ Jose Perez R._files/movimientos.js
@@ -0,0 +1,9 @@
+/* Indicador de menu responsive */
+function showmenu() {
+    document.getElementById("otroModo").classList.toggle("show");
+}
+
+/* Menu scroll
+const scroll = new SmoothScroll('.menu a[href*="#"]', {
+    speed: 800
+});*/
